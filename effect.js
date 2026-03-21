47334153effd056efe660a96d2b4eb675975b315
zyvjqk4rz5-ui <zyvjqk4rz5@privaterelay.appleid.com>
2026-03-21 21:19:07 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -159,7 +159,7 @@ $('document').ready(function(){
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+		$(this).fadeOut('slow').delay(10000).promise().done(function(){
 			$('#story').fadeIn('slow');
 		});
 	});
